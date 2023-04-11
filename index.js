67c15f1b70f500a009ce3e05bad6d7f727c2701d
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-04-11 13:45:27 -0400

    Completes arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+let multiply = (num1*num2)
 
-// Write your code here
+let random = Math.round(Math.random() * 10) + 1;
+
+const num3 = 9
+const num4 = 5
+
+let mod = num3%num4
+
+let max = Math.max(1, 8, 6, 17, 20)
